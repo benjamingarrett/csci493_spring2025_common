ff447b5bc81a3228cd63154eb3de417b13a405aa
Benjamin Garrett <bgarrett@temple.edu>
2025-01-30 21:57:42 -0500

    Added comments to PriceDiscounts

--- PriceDiscounts.scala
+++ PriceDiscounts.scala
@@ -1,13 +1,13 @@
 object PriceDiscounts extends App {
 
+  // These are both higher-order functions
   def applyDiscountUncurried(discount: Double => Double, price: Double): Double = discount(price)
-
   def applyDiscountCurried(discount: Double => Double)(price: Double): Double = discount(price)
 
+  // This is an example of partial application
   val discounter: Double => Double = applyDiscountCurried((price: Double) => price * 0.5)
 
   val originalPrice = 100.0
-
   val reducedPrice: Double = discounter(originalPrice)
 
   println(s"The original price was ${originalPrice}")
